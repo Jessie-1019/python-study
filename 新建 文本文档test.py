b0f34cf1b02e494c3d55363f09cb7289ccde144b
Jessie-1019 <2943866974@qq.com>
2026-07-12 17:26:22 +0800

    Add files via upload

---
+++ 新建 文本文档test.py
@@ -0,0 +1,5 @@
+C:\Users\27905>python
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> print("how are you")
+how are you
